f8d421e05f60217b1d46fb7828750849ab4ab577
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-02-26 15:11:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+- scrollUp
+- addDate
+- 
---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+ import addDate from "./modules/addDate";
+ import scrollUp from "./modules/scrollUp";
+
+
+
+
+
+'use strict';
+ 
+window.addEventListener('DOMContentLoaded', () => {
+// getting today's date and displaying it on a page in Russian or English (HTML and JavaScript) <div class="date"></div> 
+// addDate();
+
+// go to top button with scroll indicator and smooth scrolling to the top
+ scrollUp();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
