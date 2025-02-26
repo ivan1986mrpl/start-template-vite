6d652fdce1636d418e37bd707506a7d46feafb4f
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-02-26 15:25:38 +0200

    feat: timer-countdown

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
  import addDate from "./modules/addDate";
  import scrollUp from "./modules/scrollUp";
+ import { initCountdown } from "./modules/src/js/modules/timer-countdown";
 
 
 
@@ -12,7 +13,13 @@ window.addEventListener('DOMContentLoaded', () => {
 // addDate();
 
 // go to top button with scroll indicator and smooth scrolling to the top
- scrollUp();
+// scrollUp();
+
+// для каждого нового вызова передавать класс таймера, конечную дату в формате '29 Jun 2025 17:28' и сообщение об окончании таймера 'The timer is over'
+// Передача конечной даты
+// initCountdown('.countdown', '27 Jun 2025 12:30', 'The timer is over');
+// Передача количества секунд (сутки = 86400с) (1 час = 3600секунд)
+// initCountdown('.countdown', 86440, 'The timer is over'); // Таймер на 1 день (86400 секунд)
 
 
 
