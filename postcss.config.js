ebe4669977036b894739a534a89a89231e107964
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-07-05 15:54:50 +0300

    feat: add plugin pxtorem

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
 import autoprefixer from 'autoprefixer';
+import pxtorem from 'postcss-pxtorem';//https://www.npmjs.com/package/postcss-pxtorem
 
 export default {
-	plugins: [
-		autoprefixer(),
-	]
+  plugins: [
+    autoprefixer(),
+    pxtorem({  // Используем pxtorem как функцию с конфигурацией
+      rootValue: 16,
+      propList: ["*"], // Преобразовывать все свойства
+      selectorBlackList: [], // Селекторы, которые нужно исключить из преобразования
+      replace: true, // Заменять px на rem
+      mediaQuery: true, // Преобразовывать px внутри media queries
+      minPixelValue: 0, // Минимальное значение px для преобразования
+    })
+  ]
 };
