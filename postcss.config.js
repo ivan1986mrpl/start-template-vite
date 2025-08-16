27cb590f5719951fc00e0e3deee2aae175b5f2d7
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-08-16 14:31:18 +0300

    fix: postcss,config

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ import pxtorem from "postcss-pxtorem";
 const isProduction = process.env.NODE_ENV === "production";
 
 export default {
-  plugins: [
+  plugins: [//работают только при билде
     isProduction && autoprefixer(),
     isProduction &&
       pxtorem({  // Используем pxtorem как функцию с конфигурацией
@@ -17,22 +17,3 @@ export default {
       }),
   ].filter(Boolean), // удаляем `false`
 };
-
-/* рабочий вариант, но работает и в дев режиме
-import autoprefixer from 'autoprefixer';
-import pxtorem from 'postcss-pxtorem';//https://www.npmjs.com/package/postcss-pxtorem
-
-export default {
-  plugins: [
-    autoprefixer(),
-    pxtorem({  // Используем pxtorem как функцию с конфигурацией
-      rootValue: 16,
-      propList: ["*"], // Преобразовывать все свойства
-      selectorBlackList: [], // Селекторы, которые нужно исключить из преобразования
-      replace: true, // Заменять px на rem
-      mediaQuery: true, // Преобразовывать px внутри media queries
-      minPixelValue: 0, // Минимальное значение px для преобразования
-    })
-  ]
-};
-*/
